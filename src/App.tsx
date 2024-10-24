3f413438f9e15417d40e393e094bcfb79253669a
Aleks Zelenskiy <dezion777@gmail.com>
2024-10-24 10:00:44 +0300

    useState, onOff component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./components/Rating/Rating";
-import Accordion from "./components/Accordion/Accordion";
 import OnOff from "./components/OnOff/OnOff";
 
 function App() {
     return (
         <div className="App">
+{/*
             <Accordion titleValue={'Menu'} collapsed={true}/>
             <Accordion titleValue={'Users'} collapsed={false}/>
 
@@ -16,8 +15,9 @@ function App() {
             <Rating value={3} />
             <Rating value={4} />
             <Rating value={5} />
+*/}
 
-            <OnOff isOn={true} />
+            <OnOff />
         </div>
     );
 }
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,18 +1,42 @@
-import styles from './OnOff.module.css'
 import {useState} from "react";
 
-type OnOffPropsType = {
-    isOn: boolean
-}
+const OnOff = () => {
+    const [isOn, setIsOn] = useState<boolean>(false);
 
-const OnOff = ({ isOn }: OnOffPropsType) => {
-    const [on, setIsOn] = useState<boolean>(isOn)
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        backgroundColor: isOn ? 'green' : 'white',
+        cursor: 'pointer'
+    };
+
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '2px',
+        backgroundColor: isOn ? 'white' : 'red',
+        cursor: 'pointer'
+    };
+
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: isOn ? 'green' : 'red'
+    }
 
     return (
         <div>
-            <button className={on ? styles.on : ''} onClick={() => setIsOn(true)}>ON</button>
-            <button className={!on ? styles.off : ''} onClick={() => setIsOn(false)}>OFF</button>
-            <div className={`${styles.indicator} ${on ? styles.on : styles.off}`}></div>
+            <div onClick={() => setIsOn(true)} style={onStyle}>on</div>
+            <div onClick={() => setIsOn(false)} style={offStyle}>of</div>
+            <div style={indicatorStyle}></div>
         </div>
     );
 };
