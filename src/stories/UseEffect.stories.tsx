d24efafece528ed0bd916bd8d2862e84fe09456d
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-04 08:40:56 +0300

    UseEffect basic concepts

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,8 +10,22 @@ export const SimpleExample = () => {
     console.log('SimpleExample')
 
     useEffect(() => {
-        console.log('useEffect')
+        console.log('useEffect every render')
+        //api.getUsers().then
+        //setInterval
+        //indexedDB
+        //document.getElementById
+        //document.title = "User"
     })
+
+    useEffect(() => {
+        console.log('useEffect only first render')
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change')
+    }, [counter])
+
 
     return <>
         Hello, {counter}
