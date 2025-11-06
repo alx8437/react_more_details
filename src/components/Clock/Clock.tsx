b8f28a43427b5502561fd85c55bcd8800a9bffce
Aleks Zelenskiy <dezion777@gmail.com>
2025-11-06 10:43:10 +0300

    move analog and digital clocks to separate components

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,8 +1,12 @@
 import {useEffect, useState} from "react";
 import './clock.css'
+import DigitalClock from "./DigitalClock";
+import AnalogClock from "./AnalogClock";
 
-const get2DigitString = (num: number) => {
-    return num < 10 ? "0" + num : num
+export type ClockComponentsPropsType = {
+    hours: number
+    minutes: number
+    seconds: number
 }
 
 type ClockPropsType = {
@@ -20,52 +24,14 @@ export const Clock = (props: ClockPropsType) => {
         return () => clearInterval(intervalId)
     }, [])
 
-    const hours = get2DigitString(date.getHours());
-    const minutes = get2DigitString(date.getMinutes());
-    const seconds = get2DigitString(date.getSeconds());
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
     return (
         mode === 'digital'
-            ?
-            <div>
-                <span>{hours}</span>:
-                <span>{minutes}</span>:
-                <span>{seconds}</span>
-            </div>
-            :
-            <>
-                <div className="clock">
-                    <div
-                        className="hour_hand"
-                        style={{
-                            transform: `rotateZ(${date.getHours() * 30}deg)`
-                        }}
-                    />
-                    <div
-                        className="min_hand"
-                        style={{
-                            transform: `rotateZ(${date.getMinutes() * 6}deg)`
-                        }}
-                    />
-                    <div
-                        className="sec_hand"
-                        style={{
-                            transform: `rotateZ(${date.getSeconds() * 6}deg)`
-                        }}
-                    />
-                    <span className="twelve">12</span>
-                    <span className="one">1</span>
-                    <span className="two">2</span>
-                    <span className="three">3</span>
-                    <span className="four">4</span>
-                    <span className="five">5</span>
-                    <span className="six">6</span>
-                    <span className="seven">7</span>
-                    <span className="eight">8</span>
-                    <span className="nine">9</span>
-                    <span className="ten">10</span>
-                    <span className="eleven">11</span>
-                </div>
-            </>
+            ? <DigitalClock hours={hours} minutes={minutes} seconds={seconds}/>
+            : <AnalogClock hours={hours} seconds={seconds} minutes={minutes} />
+
     )
 }
---
+++ src/components/Clock/DigitalClock.tsx
@@ -0,0 +1,19 @@
+import {ClockComponentsPropsType} from "./Clock";
+
+const get2DigitString = (num: number) => {
+    return num < 10 ? "0" + num : num
+}
+
+export const DigitalClock = (props: ClockComponentsPropsType) => {
+    const {hours, minutes, seconds} = props
+
+    return (
+        <div>
+            <span>{get2DigitString(hours)}</span>:
+            <span>{get2DigitString(minutes)}</span>:
+            <span>{get2DigitString(seconds)}</span>
+        </div>
+    );
+};
+
+export default DigitalClock;
